0f62e56f48fa83692e0400813b9b653c5d4551ca
trevsm <trevsm@pm.me>
2022-09-25 15:52:26 -0600

    added files

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:16
+RUN apt-get update
+
+# install golang
+RUN curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+RUN tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
+ENV PATH $PATH:/usr/local/go/bin
+
+# go install pup
+RUN go install github.com/ericchiang/pup@latest
+ENV PATH $PATH:/root/go/bin
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+EXPOSE 80
+
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,63 @@
+const { exec } = require("child_process");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(
+    cors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
+
+const port = 80;
+
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == "object") {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
+
+app.post("/quizlet", (req, res) => {
+    exec(
+        `curl -sA "Chrome" -L "http://www.google.com/search?q=\"${res.body}\"+\"site:quizlet.com\"" | pup ":parent-of(:parent-of(a[href*="http"])) json{}"`,
+        (error, stdout) => {
+            if (error) {
+                res.status(500).send(error);
+            } else {
+                const data = JSON.parse(stdout).filter(
+                    (x) =>
+                        x.children.length == 2 &&
+                        JSON.stringify(x).includes("quizlet.com")
+                );
+                const final = [];
+                data.map((x) => {
+                    const entry = {};
+                    const text = getValues(x, "text").filter(
+                        (x) => x.length > 20
+                    );
+                    entry.title = text[0];
+                    entry.tags = text[1];
+                    entry.description = text[2];
+                    entry.href = getValues(x, "href")[0].replace("/url?q=", "");
+                    final.push(entry);
+                });
+                res.json(final);
+            }
+        }
+    );
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
