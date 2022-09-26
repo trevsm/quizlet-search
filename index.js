7bb223fcae04bc2ecf3ad97420ab510549ccaaaf
Trevor Smith <trevorsm@pm.me>
2022-09-25 18:40:50 -0600

    test

--- index.js
+++ index.js
@@ -8,18 +8,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-    );
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+//     );
+//     next();
+// });
+
+app.use(cors());
 
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("<b>hi</b>");
+});
 
 function getValues(obj, key) {
     var objects = [];
